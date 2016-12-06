f4e40d9b1953b3e4192ef77f9d999d7bb3783b2a
jackierios <rc.rios.jacqueline@leadps.org>
2016-12-06 17:13:23 +0000

    added new version

--- class_samples/extra/quest2.py
+++ class_samples/extra/quest2.py
@@ -3,15 +3,16 @@
 import random
 # creates a random number between 3 and 567 - edit as necessary
 myNum = random.randint(1, 1000)
-
-print("I'm thinking of a number between 1 and 1000. Enter a guess!") 
-guess= int(raw_input())
-if guess < 2: 	
-	print("Nope. Sorry too low")
-if guess >= 3:
-	print("Nope. Sorry too high")
-if guess == 2: 
-	print("YEAH! YOU WON!")
+wrong = 0
+print("I'm thinking of a number between 1 and 1000. Enter a guess!")  
+while wrong < myNum: 
+	guess= int(raw_input())
+	if guess < myNum: 	
+		print("Nope. Sorry too low. Guess again")
+	if guess > myNum:
+		print("Nope. Sorry too high. GUess again")
+	if guess == myNum: 
+		print("YEAH! YOU WON!")
 
 
 
